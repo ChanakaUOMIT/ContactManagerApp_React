b2bc8cca464fd9e00c32249ad9d9991191acaa54
Chanaka Sampath Wickramanayaka <chanakauomfit@gmail.com>
2018-10-23 07:02:08 +0530

    Updates

---
+++ precache-manifest.efc6219817132ffbfef889f7327e039f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47e073f320448ef9c718",
+    "url": "/ContactManagerApp_React/static/js/runtime~main.47e073f3.js"
+  },
+  {
+    "revision": "fa439adee46aac72979b",
+    "url": "/ContactManagerApp_React/static/js/main.fa439ade.chunk.js"
+  },
+  {
+    "revision": "c095438c849a5c563c88",
+    "url": "/ContactManagerApp_React/static/js/1.c095438c.chunk.js"
+  },
+  {
+    "revision": "fa439adee46aac72979b",
+    "url": "/ContactManagerApp_React/static/css/main.b5795ca4.chunk.css"
+  },
+  {
+    "revision": "c095438c849a5c563c88",
+    "url": "/ContactManagerApp_React/static/css/1.18aadd47.chunk.css"
+  },
+  {
+    "revision": "f7b4a39aabd99cd15ff7bb0e1746d684",
+    "url": "/ContactManagerApp_React/index.html"
+  }
+];
